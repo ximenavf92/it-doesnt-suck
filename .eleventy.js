285608df8f69785d79a759c57ab7d733ca8dc12b
Ximena Vila Ferral <hola@ximenavf.com>
2019-10-28 01:33:21 -0500

    update eleventy.js settings

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function(eleventyConfig) {
 
-    eleventyConfig.addPassthroughCopy("/assets");
+    eleventyConfig.addPassthroughCopy("src/assets");
 
     return {
         passthroughFileCopy: true,
