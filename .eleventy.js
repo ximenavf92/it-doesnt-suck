94c2ac15289a267e70b5c8c873ce5290a00860e9
Ximena Vila Ferral <hola@ximenavf.com>
2019-10-28 01:18:55 -0500

    update eleventy.js settings

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,14 @@
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addPassthroughCopy("assets/fonts");
+    eleventyConfig.addPassthroughCopy("assets/css");
+    eleventyConfig.addPassthroughCopy("assets/images");
 
     return {
         passthroughFileCopy: true,
         markdownTemplateEngine: "njk",
-        templateFormats: ["html", "njk", "md", "css", "img"],
+        templateFormats: ["html", "njk", "md", "css"],
         dir: {
             input: "src",
             output: "_site",
